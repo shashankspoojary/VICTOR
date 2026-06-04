ba590b5414ae29daa1c54cf08d64d904dbc0b936
Shashank <your-github-email@example.com>
2026-06-04 19:08:43 +0530

    Remove all files from repository

--- app/models.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class ChatRequest(BaseModel):
-    message: str
-    session_id: Optional[str] = None
-    tts: bool = False
-    imgbase64: Optional[str] = None
